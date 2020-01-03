ca5691728fc4a13ea873bb787edbc19c1af0b89c
Try <khovct@gmail.com>
2020-01-02 23:09:56 -0800

    finished the stack problem

--- W3D5/stack.rb
+++ W3D5/stack.rb
@@ -1,24 +1,37 @@
+class StackNode
+
+  attr_reader :data
+  attr_accessor :next
+
+  def initialize(data)
+    @data = data
+    @next = nil
+  end
+
+end
+
+
 class Stack
 
-    attr_accessor :head, :next
+    attr_accessor :head
 
     def initialize
       # create ivar to store stack here!
       @head = nil
-      @next = nil
     end
 
     def push(el)
       # adds an element to the stack
-      self.next = self.head
-      self.head = el
+      stack_node = StackNode.new(el) # create a new stack node for the element
+      stack_node.next = self.head # the new object is on top of the stack and below it is the current head of the stack
+      self.head = stack_node # the new head of the stack is the new object
     end
 
     def pop
       # removes one element from the stack
-      hold = self.next
-      self.head = self.next
-      self.next = hold
+      hold = self.head
+      self.head = self.head.next # the head is the object below the current one
+      hold # we return the value at hold
     end
 
     def peek
@@ -27,13 +40,13 @@ class Stack
     end
   end
 
-p stack = Stack.new
+stack = Stack.new
 stack.push(1)
+p stack.peek.data
 stack.push(2)
 stack.push(3)
 
-p stack.peek
+p stack.peek.data
 stack.pop
-p stack.peek
 stack.pop
-p stack.peek
+p stack.peek.data
